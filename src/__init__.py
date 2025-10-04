d59d93c867e101aa9dfe03847a82bf3b6beea1fa
Faithlix Mark Macapagal <faithlixmarkmacapagal@gmail.com>
2025-10-04 13:11:20 +0800

    src files
    Upload

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+"""
+//------------------------------------------------------------
+Title: __init_.py
+Author: Mark Macapagal
+Description: Initialization file for src package
+Date: June 2024
+Version: 1.0
+Changes: Initial release
+Note: This code requires the src folder containing k_dae.py, autoencoder.py, and
+      utils.py files in the same directory.               
+//------------------------------------------------------------
+
+"""
